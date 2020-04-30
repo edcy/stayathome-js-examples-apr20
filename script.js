80b0a41e6841d08dd26b229128afbab9fbcd3756
edcy <64536254+edcy@users.noreply.github.com>
2020-04-30 17:10:49 +0200

    Push first js

--- script.js
+++ script.js
@@ -1,1 +1,178 @@
 console.log('Script file is loaded.');
+
+// Single line comment
+
+/*
+Multiple
+line
+comments
+*/
+
+/* First part:
+1+1
+2
+12-7
+5
+4*3
+12
+15/6
+2.5
+let age;
+undefined
+age = 34
+34
+age +5
+39
+age = 48/8
+6
+age+5
+11
+let numberOfLegs
+undefined
+let numb
+undefined
+let numberOfLegs = 4
+undefined
+numberOfLegs * 2
+8
+let name = Edina
+VM490:1 Uncaught ReferenceError: Edina is not defined
+    at <anonymous>:1:12
+(anonymous) @ VM490:1
+age / 3
+2
+anotherAge / 3
+VM543:1 Uncaught ReferenceError: anotherAge is not defined
+    at <anonymous>:1:1
+(anonymous) @ VM543:1
+name = "Edina"
+VM576:1 Uncaught ReferenceError: Cannot access 'name' before initialization
+    at <anonymous>:1:6
+(anonymous) @ VM576:1
+let name = "Edina"
+undefined
+name
+"Edina"
+name = "Egg"
+"Egg"
+name = 'StayAtHome'
+"StayAtHome"
+let sentence = "That person's cat."
+undefined
+sentence = 'He said okay'
+"He said okay"
+let backtick = 'backtick'
+undefined
+let oldEnoughToDrive = true
+undefined
+34 +5 
+39
+'Hello' + nema
+VM1066:1 Uncaught ReferenceError: nema is not defined
+    at <anonymous>:1:11
+(anonymous) @ VM1066:1
+'Hello,' + name
+"Hello,StayAtHome"
+'Hello,' + name + 43 + 'another string'
+"Hello,StayAtHome43another string"
+'11' + '23'
+"1123"
+true && true
+true
+true && false
+false
+false && false
+false
+true || false
+true
+"true && (false || (true && false))"
+"true && (false || (true && false))"
+true && (false || (true && false))
+
+false
+let post = "This is my first post"
+undefined
+let post2 = "This is my second post"
+undefined
+let color1 = "orange"
+undefined
+let color2 = "blue"
+undefined
+let colors = ["orange", "blue", "green"]
+undefined
+colors
+(3) ["orange", "blue", "green"]
+colors [0]
+"orange"
+colors [2]
+"green"
+let pickedColor = 2
+undefined
+colors
+(3) ["orange", "blue", "green"]
+colors[pickedColor]
+"green"
+colors[1]
+"blue"
+colors.lenght
+undefined
+colors.length
+3
+name.length
+10
+colors[3]
+undefined
+colors [3] = 'lime'
+"lime"
+colors
+(4) ["orange", "blue", "green", "lime"]
+colors.push('purple')
+5
+colors
+(5) ["orange", "blue", "green", "lime", "purple"]
+colors[colors.length] = 'cyan'
+"cyan"
+colors
+(6) ["orange", "blue", "green", "lime", "purple", "cyan"]
+colors.push(colors)
+7
+colors
+(7) ["orange", "blue", "green", "lime", "purple", "cyan", Array(7)]
+let norGoodExample = ['text', 123, true]
+undefined
+let person = {
+    name: 'Adam', 
+    age: 34,
+    favriteColors: colors,
+    
+VM2583:5 Uncaught SyntaxError: Unexpected end of input
+let person = {
+    name: 'Adam', 
+    age: 34,
+    favoriteColors: colors,
+    oldEnoughToDrive: gtrue}
+
+VM2617:5 Uncaught ReferenceError: gtrue is not defined
+    at <anonymous>:5:23
+(anonymous) @ VM2617:5
+let person = {
+    name: 'Adam', 
+    age: 34,
+    favoriteColors: colors,
+    oldEnoughToDrive: true}
+undefined
+person
+{name: "Adam", age: 34, favoriteColors: Array(7), oldEnoughToDrive: true}
+'Hello, dear ' + person.name
+"Hello, dear Adam"
+person.name = 'Peti'
+"Peti"
+person
+{name: "Peti", age: 34, favoriteColors: Array(7), oldEnoughToDrive: true}
+person.food = pizza
+VM2843:1 Uncaught ReferenceError: pizza is not defined
+    at <anonymous>:1:15
+(anonymous) @ VM2843:1
+person.food = 'pizza'
+"pizza"
+*/
